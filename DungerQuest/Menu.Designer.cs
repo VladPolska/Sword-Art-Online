dda6cb91f38899b6a1a2297f918cf9a0324260a9
VladPolska <62495309+VladPolska@users.noreply.github.com>
2020-04-02 12:40:27 +0300

    Why Not

---
+++ DungerQuest/Menu.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace DungerQuest
+{
+    partial class Menu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Menu));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.button1.Image = ((System.Drawing.Image)(resources.GetObject("button1.Image")));
+            this.button1.Location = new System.Drawing.Point(274, 143);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(300, 50);
+            this.button1.TabIndex = 0;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button2.ForeColor = System.Drawing.SystemColors.Control;
+            this.button2.Image = ((System.Drawing.Image)(resources.GetObject("button2.Image")));
+            this.button2.Location = new System.Drawing.Point(274, 224);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(300, 50);
+            this.button2.TabIndex = 1;
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(721, 387);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(82, 68);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.label1.Cursor = System.Windows.Forms.Cursors.No;
+            this.label1.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.label1.Font = new System.Drawing.Font("Kristen ITC", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(718, 371);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(83, 16);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "By VladPolska";
+            // 
+            // Menu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Menu";
+            this.Text = "Menu";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ DungerQuest/New_Game.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DungerQuest
+{
+    public partial class New_Game : Form
+    {
+        int n = 20, att = 0, prot = 0, atl = 0, skill = 0;
+        string dia_1, Name;
+        public New_Game()
+        {
+            InitializeComponent();
+            label2.Hide();
+            label3.Hide();
+            label4.Hide();
+            label5.Hide();
+            button2.Hide();
+            button3.Hide();
+            button4.Hide();
+            button5.Hide();
+            button6.Hide();
+            button7.Hide();
+            button8.Hide();
+            button10.Hide();
+            button11.Hide();
+            button12.Hide();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Name = textBox1.Text;
+            textBox1.Hide();
+            button1.Hide();
+            dia_1 = "Приветствую " + Name + " в нашей армии. Скажи о своих характеристиках";
+            label1.Text = dia_1;
+            label2.Show();
+            label3.Show();
+            label4.Show();
+            label5.Show();
+            button2.Show();
+            button3.Show();
+            button4.Show();
+            button5.Show();
+            button6.Show();
+            button7.Show();
+            button8.Show();
+            label2.Text = "Очки умений: " + n;
+            label3.Text = "Атака: " + att;
+            label4.Text = "Защита: " + prot;
+            label5.Text = "Ловкость: " + atl;
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (n != 0)
+            {
+                att = att + 1;
+                n = n - 1;
+            }
+            label2.Text = "Очки умений: " + n;
+            label3.Text = "Атака: " + att;
+            label4.Text = "Защита: " + prot;
+            label5.Text = "Ловкость: " + atl;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (att != 0)
+            {
+                att = att - 1;
+                n = n + 1;
+            }
+            label2.Text = "Очки умений: " + n;
+            label3.Text = "Атака: " + att;
+            label4.Text = "Защита: " + prot;
+            label5.Text = "Ловкость: " + atl;
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (n != 0)
+            {
+                prot = prot + 1;
+                n = n - 1;
+            }
+            label2.Text = "Очки умений: " + n;
+            label3.Text = "Атака: " + att;
+            label4.Text = "Защита: " + prot;
+            label5.Text = "Ловкость: " + atl;
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            button10.Hide();
+            button11.Hide();
+            button12.Hide();
+            att = att + 2;
+            prot = prot + 1;
+            atl = atl + 4;
+            skill = 3;
+            dia_1 = "Ассасин - скрытный класс. Довольно хороший выбор";
+            label1.Text = dia_1;
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            button10.Hide();
+            button11.Hide();
+            button12.Hide();
+            att = att + 5;
+            atl = atl + 2;
+            skill = 2;
+            dia_1 = "Лучник - класс для точных. Если умеешь стрелять, то этот класс для тебя";
+            label1.Text = dia_1;
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            button10.Hide();
+            button11.Hide();
+            button12.Hide();
+            att = att + 3;
+            prot = prot + 5;
+            atl = atl - 2;
+            skill = 1;
+            dia_1 = "Мечник - самый стандартный класс. Если любишь ходить в тяжелой броне, мечник для тебя";
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            if (prot != 0)
+            {
+                prot = prot - 1;
+                n = n + 1;
+            }
+            label2.Text = "Очки умений: " + n;
+            label3.Text = "Атака: " + att;
+            label4.Text = "Защита: " + prot;
+            label5.Text = "Ловкость: " + atl;
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            if (n != 0)
+            {
+                atl = atl + 1;
+                n = n - 1;
+            }
+            label2.Text = "Очки умений: " + n;
+            label3.Text = "Атака: " + att;
+            label4.Text = "Защита: " + prot;
+            label5.Text = "Ловкость: " + atl;
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            if (atl != 0)
+            {
+                atl = atl - 1;
+                n = n + 1;
+            }
+            label2.Text = "Очки умений: " + n;
+            label3.Text = "Атака: " + att;
+            label4.Text = "Защита: " + prot;
+            label5.Text = "Ловкость: " + atl;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (n == 0)
+            {
+                button2.Hide();
+                label2.Hide();
+                label3.Hide();
+                label4.Hide();
+                label5.Hide();
+                button2.Hide();
+                button3.Hide();
+                button4.Hide();
+                button5.Hide();
+                button6.Hide();
+                button7.Hide();
+                button8.Hide();
+                button10.Show();
+                button11.Show();
+                button12.Show();
+                dia_1 = "Молодец" + Name + ", хорошие характеристики. Теперь выбери свой класс";
+                label1.Text = dia_1;
+            }
+        }
+    }
+}
